e47db167220266635d2742724bc03da8e2824140
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-07-02 21:30:48 +0200

    migration from previous project

---
+++ ShopMVC.ViewModels/SubviewViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopMVC.ViewModels
+{
+    public class SubviewViewModel
+    {
+        public string TargetView { get; set; }
+        public object TargetViewModel { get; set; }
+    }
+}
