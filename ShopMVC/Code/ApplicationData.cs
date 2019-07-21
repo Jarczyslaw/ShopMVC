684a79d608e19109818f6c9afa5750738ae85fe1
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-07-21 18:08:12 +0200

    dev

---
+++ ShopMVC/Code/ApplicationData.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+
+namespace ShopMVC.Code
+{
+    public class ApplicationData
+    {
+        private readonly HttpApplicationState application;
+        private readonly object sync = new object();
+
+        public ApplicationData(HttpApplicationState application)
+        {
+            this.application = application;
+        }
+
+        public int SessionsCount
+        {
+            get => (int)application[nameof(SessionsCount)];
+            set => application[nameof(SessionsCount)] = value;
+        }
+
+        public void ResetSessions()
+        {
+            lock (sync)
+            {
+                SessionsCount = 0;
+            }
+        }
+
+        public int AddSession()
+        {
+            lock (sync)
+            {
+                SessionsCount++;
+                return SessionsCount;
+            }
+        }
+
+        public int RemoveSession()
+        {
+            lock (sync)
+            {
+                SessionsCount--;
+                return SessionsCount;
+            }
+        }
+    }
+}
--- ShopMVC/Global.asax.cs
+++ ShopMVC/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using ShopMVC.App_Start;
+using ShopMVC.Code;
 using ShopMVC.Services;
 using System;
+using System.Diagnostics;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -13,21 +15,29 @@ namespace ShopMVC
     {
         private readonly Lazy<ILoggerService> logger = new Lazy<ILoggerService>(() => UnityConfig.Container.Resolve<ILoggerService>());
         private ILoggerService LoggerInstance => logger.Value;
+        private readonly Lazy<ApplicationData> applicationData = new Lazy<ApplicationData>(() => new ApplicationData(HttpContext.Current.Application));
+        private ApplicationData ApplicationDataInstance => applicationData.Value;
 
         protected void Application_Start()
+        {
+            ApplicationSetup();
+
+            ApplicationDataInstance.ResetSessions();
+            LogInfo(nameof(Application_Start));
+        }
+
+        private void ApplicationSetup()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterFilters(GlobalFilters.Filters);
             ViewEnginesConfig.Setup();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            LoggerInstance.Info(nameof(Application_Start));
         }
 
         protected void Application_End()
         {
-            LoggerInstance.Info(nameof(Application_End));
+            LogInfo(nameof(Application_End));
         }
 
         protected void Application_Error()
@@ -37,6 +47,22 @@ namespace ShopMVC
             {
                 LoggerInstance.Fatal(exception, "Application error");
             }
+        }
+
+        protected void Session_Start()
+        {
+            LogInfo($"Session started. ID: {HttpContext.Current.Session.SessionID}. Sessions count: {ApplicationDataInstance.AddSession()}");
+        }
+
+        protected void Session_End()
+        {
+            LogInfo($"Session ended. ID: {HttpContext.Current.Session.SessionID}. Sessions count: {ApplicationDataInstance.AddSession()}");
+        }
+
+        private void LogInfo(string message)
+        {
+            LoggerInstance.Info(message);
+            Debug.WriteLine(message);
         }
     }
 }
