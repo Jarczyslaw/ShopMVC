d117e143b45eb2f15a4d99f077a04619be62c6ac
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-07-22 21:24:08 +0200

    added MVC sitemap

---
+++ ShopMVC/Sitemap/CategoriesNodeProvider.cs
@@ -0,0 +1,33 @@
+﻿using MvcSiteMapProvider;
+using ShopMVC.Services;
+using System.Collections.Generic;
+using Unity;
+
+namespace ShopMVC.Sitemap
+{
+    public class CategoriesNodeProvider : DynamicNodeProviderBase
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoriesNodeProvider()
+        {
+            categoriesService = UnityConfig.Container.Resolve<ICategoriesService>();
+        }
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var nodes = new List<DynamicNode>();
+            foreach (var category in categoriesService.GetAll())
+            {
+                var newNode = new DynamicNode()
+                {
+                    Title = category.Title,
+                    Key = "Category_" + category.CategoryId,
+                };
+                newNode.RouteValues.Add("categoryId", category.CategoryId);
+                nodes.Add(newNode);
+            }
+            return nodes;
+        }
+    }
+}
---
+++ ShopMVC/Sitemap/CoursesNodeProvider.cs
@@ -0,0 +1,34 @@
+﻿using MvcSiteMapProvider;
+using ShopMVC.Services;
+using System.Collections.Generic;
+using Unity;
+
+namespace ShopMVC.Sitemap
+{
+    public class CoursesNodeProvider : DynamicNodeProviderBase
+    {
+        private readonly ICoursesService coursesService;
+
+        public CoursesNodeProvider()
+        {
+            coursesService = UnityConfig.Container.Resolve<ICoursesService>();
+        }
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var nodes = new List<DynamicNode>();
+            foreach (var course in coursesService.GetAll())
+            {
+                var newNode = new DynamicNode()
+                {
+                    Title = course.Title,
+                    Key = "Course_" + course.CourseId,
+                    ParentKey = "Category_" + course.CategoryId,
+                };
+                newNode.RouteValues.Add("courseId", course.CourseId);
+                nodes.Add(newNode);
+            }
+            return nodes;
+        }
+    }
+}
