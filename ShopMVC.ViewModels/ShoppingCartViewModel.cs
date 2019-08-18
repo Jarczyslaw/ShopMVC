21fe29a86a3044ec36df2fb71bf3953c5c8cce12
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-08-18 09:59:11 +0200

    dev

--- ShopMVC.ViewModels/ShoppingCartViewModel.cs
+++ ShopMVC.ViewModels/ShoppingCartViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace ShopMVC.ViewModels
+﻿using ShopMVC.Commons.Extensions;
+using ShopMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopMVC.ViewModels
 {
     public class ShoppingCartViewModel : BaseViewModel
     {
+        public IEnumerable<ShoppingCartPosition> Positions { get; set; }
+        public decimal Value { get; set; }
+        public string ValueDisp => Value.ToCurrency();
+        public int PositionsCount => Positions?.Count() ?? 0;
     }
 }
